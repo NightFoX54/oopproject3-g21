e4db4ccdd8ea68ee8bd85e86d24f359a0fe5b0b6
Berkedgnn <berkedogan_2001@hotmail.com>
2025-01-13 19:41:01 +0300

    Update cashier.java

--- src/cashier.java
+++ src/cashier.java
@@ -1,5 +1,17 @@
+/**
+ * The {@code cashier} class represents a cashier in the system.
+ * It extends the {@code user} class, inheriting its properties and behavior.
+ */
 class cashier extends user{
 
+    /**
+     * Constructs a new {@code cashier} object with the specified name, surname, username, and role.
+     *
+     * @param name     The first name of the cashier.
+     * @param surname  The last name of the cashier.
+     * @param username The username of the cashier.
+     * @param role     The role of the cashier in the system.
+     */
     cashier(String name, String surname, String username, String role) {
         super(name, surname, username, role);
     }
